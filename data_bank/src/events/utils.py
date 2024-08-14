b33966495f4682b0408304a2a5d23f4550a0e59a
Alex <polonnikov.alexander@gmail.com>
2024-08-14 16:23:12 +0300

    commit

--- data_bank/src/events/utils.py
+++
@@ -1,7 +0,0 @@
-import os.path
-
-
-def create_directory(folder_name: str) -> None:
-    path: str = os.path.join(os.getcwd(), folder_name)
-    if os.path.exists(path) is False:
-        os.mkdir(path)
